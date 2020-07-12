43442309ae9ca5daa865f89b2f98bf0e395c1f79
sami-aloosi <kingxsam@gmail.com>
2020-07-11 22:51:35 -0700

    mvp done

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,13 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [peopleComments, setPeopleComments] = useState(props.comments)
+  console.log(peopleComments)
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {peopleComments.map(data => <Comment comment = {data}/>)}
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,23 @@
 //Complete the necessary code in this file
-// import useState
-import React from "react";
+// import useState 
+//done
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
+import dummyData from "../../dummy-data"
 // import data 
+// done
 
 const PostsPage = () => {
   // set up state for your data
+  //done
+  const [postData, setPostData] = useState(dummyData)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {/* done */}
+      {postData.map(data => <Post post = {data}  /> )}
+    
     </div>
   );
 };

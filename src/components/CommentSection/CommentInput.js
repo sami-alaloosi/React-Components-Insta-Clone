2e003996fe2af1c58267e581ad7a0b205e84d6f7
sami-aloosi <kingxsam@gmail.com>
2020-07-14 21:49:27 -0700

    strech

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -2,7 +2,9 @@
 import React from 'react';
 
 const CommentInput = props => {
+  console.log(props.comment)
   return (
+    
     <form className="comment-form" onSubmit={props.submitComment}>
       <input
         type="text"
--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -7,13 +7,24 @@ import "./Comment.css";
 const CommentSection = props => {
   // Add state for the comments
   const [peopleComments, setPeopleComments] = useState(props.comments)
-  console.log(peopleComments)
+const [value, setValue] = useState()
+const submitComment = (event) => {
+  event.preventDefault()
+  event.target.reset()
+  let copy = []
+  peopleComments.forEach(data => copy.push(data))
+  copy.push({username:"sami-AlAloosi", text :value})
+  setPeopleComments(copy)
+}
 
+const changeComment =(event) =>{
+  setValue(event.target.value)
+}
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      {peopleComments.map(data => <Comment comment = {data} key = {data.username}/>)}
-      <CommentInput />
+      {peopleComments.map(data => <Comment comment = {data} key = {data.username + data.text}/>)}
+      <CommentInput submitComment ={submitComment} changeComment={changeComment}/>
     </div>
   );
 };
